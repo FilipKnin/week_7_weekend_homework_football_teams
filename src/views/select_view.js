baf84c49a1b2b6903bf7a7ead4f0c4dfd2d94b0e
Filip Kuszenin <filipkuszenin@gmail.com>
2018-09-14 16:31:18 +0100

    SelectView populate created and tested

--- src/views/select_view.js
+++ src/views/select_view.js
@@ -11,6 +11,18 @@ SelectView.prototype.bindEvents = function () {
     const areasNames = event.detail;
     console.log('SelectView has areasNames:', areasNames);
     console.log(this);
-    this.populate();
+    this.populate(areasNames);
   });
+
+  SelectView.prototype.populate = function (areasNames) {
+    areasNames.forEach((name) => {
+      const newOption = document.createElement('option');
+      newOption.textContent = name;
+      newOption.value = name;
+      // console.log('populate has newOption:', newOption);
+      this.container.appendChild(newOption);
+    });
+  };
 };
+
+module.exports = SelectView;
