164a63346ca5db276dcba3a13a51615005df4e99
Filip Kuszenin <filipkuszenin@gmail.com>
2018-09-16 23:36:19 +0100

    Competitions.getCompetitionsObjects created and tested

--- src/models/competitions.js
+++ src/models/competitions.js
@@ -10,6 +10,15 @@ this.areasData = null
 Competitions.prototype.bindEvents = function () {
   // console.log('competitions bindEvents exists');
   this.getAreasData();
+  this.getCompetitionsData();
+
+  PubSub.subscribe('SelectView:selected-area-name', (event) => {
+    const selectedAreaName = event.detail;
+    console.log('Competitions.bindEvents has selectedAreaName:', selectedAreaName);
+    this.getCompetitionObjects(selectedAreaName);
+  });
+
+
 };
 
 Competitions.prototype.getAreasData = function () {
@@ -27,6 +36,31 @@ Competitions.prototype.getAreasData = function () {
     .catch((err) => {
       console.error(err);
     })
+  };
+
+  Competitions.prototype.getCompetitionsData = function () {
+    const url = 'http://api.football-data.org/v2/competitions/';
+      const request = new Request(url);
+      request.get({ "X-Auth-Token": API_KEY })
+      .then((data) => {
+        console.log('Competitions.getCompetitionsData has data:', data);
+        this.competitionsData = data.competitions;
+        console.log('Competitions.getCompetitionsData has this.competitionsData:', this.competitionsData);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  };
+
+  Competitions.prototype.getCompetitionObjects = function (selectedAreaName) {
+    // console.log('Competitions.getCompetitionObject has this.competitionsData:', this.competitionsData);
+    // console.log('Competitions.getCompetitionObject has selectedAreaName:', selectedAreaName);
+    // console.log(this.competitionsData[0].area.name);
+    const selectedObjects = this.competitionsData.filter((competition) => competition.area.name === selectedAreaName);
+    // console.log('Competitions.getCompetitionObject has selectedCompetitionsObjects:', selectedCompetitionsObjects);
+    PubSub.publish('Competitions:selected-competitions-objects', selectedObjects);
+    //filter
+    //publish
   };
 
 
--- src/views/select_view.js
+++ src/views/select_view.js
@@ -5,19 +5,19 @@ this.container = container;
 };
 
 SelectView.prototype.bindEvents = function () {
-  console.log('SelectView has bindEvents');
+  // console.log('SelectView has bindEvents');
   PubSub.subscribe('Competitions:all-areas-names', (event) => {
-    console.log('SelectView has event:', event);
+    // console.log('SelectView has event:', event);
     const areasNames = event.detail;
-    console.log('SelectView has areasNames:', areasNames);
-    console.log(this);
+    // console.log('SelectView has areasNames:', areasNames);
+    // console.log(this);
     this.populate(areasNames);
 
     this.container.addEventListener('change', (event) => {
-      console.log('change event.target.value:', event.target.value);
+      // console.log('change event.target.value:', event.target.value);
       const selectedArea = event.target.value;
-      console.log('change selectedArea:', selectedArea);
-      PubSub.publish('SelecView:selected-area-name', selectedArea);
+      // console.log('change selectedArea:', selectedArea);
+      PubSub.publish('SelectView:selected-area-name', selectedArea);
     })
 
   });
